32287a665f8c2d400b76242ec8cbd5282b743b19
Gr1zlY <x.grizly@gmail.com>
2010-09-17 00:30:27 +0300

    just updated comment form

--- application/frontend/views/comments/new.php
+++ application/frontend/views/comments/new.php
@@ -5,7 +5,7 @@
 	<?php echo form_open(current_url()); ?>
 		<?php echo form_input('name', set_value('name')); ?><?php echo form_label('name','name'); ?>
 		<?php $this->load->view('comments/captcha');?>
-		<?php echo form_textarea('comment', '', 'cols="90" rows="3"'); ?>
+		<textarea rows="3" name ="comment"></textarea>
 		<?php echo form_submit('submit', 'Post', 'id="send"'); ?>
 	<?php echo form_close(); ?>
 </div>
