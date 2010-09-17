fd8e5c88cb4fe6df239e4701ca46983fad2ef210
Gr1zlY <x.grizly@gmail.com>
2010-09-17 23:03:41 +0300

    Comments update for adminpanel

--- application/backend/models/comment_model.php
+++ application/backend/models/comment_model.php
@@ -6,7 +6,6 @@ class Comment_model extends Model {
 	{
 		parent::Model();
 	}
-
 	
 	function aGetComments($limit, $offset){
 	
@@ -21,18 +20,29 @@ class Comment_model extends Model {
 	}
 	
 	function aDeleteComment($id){
+
+		$comment = $this->aGetBasicComment($id);
+		$this->sDecreaseCounter($comment['post_id']);
 	
 		$this->db->where('id', (int)$id);
 		return $this->db->delete('comments');					
 	}
 
-	
-	/*function mGetCommentsCount($id){
-	
-		$this->db->where('post_id', $id);
-		$this->db->from('comments');
+	protected function aGetBasicComment($id){
+		$this->db->select('id, post_id');
+		$this->db->where('id', $id);
 		
-		return $this->db->count_all_results();
-	}*/
+		if ($query->num_rows() > 0){
+			return $query->row_array();
+		}
+		return FALSE;
+	}
+
+	protected function sDecreaseCounter($post_id){
+
+		$this->db->set('comments_qty', 'comments_qty - 1', FALSE);
+		$this->db->where('id' , $post_id);
+		$this->db->update('blog');
+	}
 }
 
