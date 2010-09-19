c08b7d6c4522a30eac5b698a8579a78510d2ea7b
Okspen <rip4eg@gmail.com>
2010-09-19 17:10:01 +0300

    Rewritten tree comments

--- application/frontend/views/comments/view.php
+++ application/frontend/views/comments/view.php
@@ -6,29 +6,54 @@
 			var current  = $(this).parent().parent().parent();
 			current.append($('.commentForm'));
 			id = $(current).attr('id').replace("comment","");
-			alert(id);
 			$('#parent-id').val(id);
 			return false;
 		});
 	});
 </script>
-
-<ul class="commentsList">
-
-	<?php if(isset($comments) AND $comments != FALSE):?>
+<?php if(isset($comments) AND $comments != FALSE):?>
 	
-		<?php foreach($comments as $comment):?>
-			<li id="comment<?php echo ($comment['parent_id'])?'child_comment ':' '; echo $comment['id']; ?>">
-				<div class="meta">
-					<span class="date"><?php echo $comment['name']; ?> at <?php echo mdate('%d.%m.%y @ %h:%i %a', $comment['time']); ?></span>
-					<span><?php echo anchor('', 'reply', 'class = "reply"');?></span>
-				</div>
-				<div class="commentContent">
-					<p><?php echo $comment['comment'];?></p>
-				</div>
-			</li>
-		<?php endforeach;?>
+<?php
+	//начинается быдлокод	
+	function ok_comment_get_children($comments, $parent_id)
+	{
+		$children = array();
+		//проходимся по всем комментариям
+		$count = count($comments);
+		for($i=0; $i<$count; $i++)
+		{
+			if($comments[$i]['parent_id'] == $parent_id)
+				$children[] = $comments[$i];
+		}
+		return $children;
+	}
+	
+	function ok_comment_draw_recursively($comments, $root)
+	{	
+		$children = ok_comment_get_children($comments, $root);
 		
-	<?php endif;?>
+		$count = count($children);
+		if($count==0) return; ?>
+		
+		<ul class="commentsList">
+		<?php	
+			for($i=0; $i<$count; $i++)
+			{
+				$comment = $children[$i]; ?> 
+			
+				<li id="comment<?php echo $comment['id'];?>">
+					<div class="meta">
+						<span class="date"><?php echo $comment['name']; ?> at <?php echo mdate('%d.%m.%y @ %h:%i %a', $comment['time']); ?></span>
+						<span><?php echo anchor('', 'reply', 'class = "reply"');?></span>
+					</div>
+					<div class="commentContent">
+						<p><?php echo $comment['comment'];?></p>
+					</div>
+				<?php ok_comment_draw_recursively($comments, $comment['id']); ?>
+				</li>
+		<?php } ?>
+		</ul>
+<?php }	?>
 	
-</ul>
+<?php ok_comment_draw_recursively($comments, 0); ?>
+<?php endif;?>
