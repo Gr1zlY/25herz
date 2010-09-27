b9068b3d657d9025d7a42c04592c25a6cb6efd31
Okspen <rip4eg@gmail.com>
2010-09-28 00:00:14 +0300

    changed email in footer

--- application/frontend/views/parts/footer.php
+++ application/frontend/views/parts/footer.php
@@ -1,1 +1,1 @@
-<p id="copyright">&copy; 2010 25 HERZ</p><p id="mailto">Есть вопросы? - <a href="mailto:rip4eg@gmail.com">rip4eg@gmail.com</a>!</p>
+<p id="copyright">&copy; 2010 25 HERZ</p><p id="mailto">Есть вопросы? - <a href="mailto:info@25herz.com">info@25herz.com</a>!</p>
